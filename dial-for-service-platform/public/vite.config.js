edc4261e16d2ee020939b31a9137f74d2ba7afee
nooralam35 <63764726+nooralam35@users.noreply.github.com>
2025-06-18 16:36:38 +0530

    Add files via upload

--- dial-for-service-platform/public/vite.config.js
+++ dial-for-service-platform/public/vite.config.js
@@ -1,20 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Use @ as alias for /src
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
   server: {
-    port: 3000,
-    open: true,
-  },
+    port: 3000
+  }
 });
